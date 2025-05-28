72a5c7916a787dfc9bc3b6abb84b726a7c2298ee
Hippolyte <hippolyte.texier--bourrouilh@epfedu.fr>
2025-05-28 17:24:10 +0200

    ca marche pas avec le rajout du recyclerview dans homefragment toute la partie onviewcrated...

--- app/src/main/java/com/terminator/ui/home/HomeFragment.kt
+++ app/src/main/java/com/terminator/ui/home/HomeFragment.kt
@@ -1,26 +1,20 @@
 package com.terminator.ui.home
 
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowInsets
-import android.view.WindowInsetsController
-import android.widget.TextView
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.terminator.DetailArticle
 import com.terminator.PRODUCT_DATA
 import com.terminator.ProductAdapter
-import com.terminator.R
 import com.terminator.databinding.FragmentHomeBinding
 import com.terminator.repository.ProductRepository
 import kotlinx.coroutines.runBlocking
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -37,12 +31,16 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        return root
+    }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         val recyclerView =binding.listArticles
 
         recyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL,false)
-        runBlocking {
+        lifecycleScope.launch {
             val repository = ProductRepository()
             val products = repository.getAllProducts()
 
@@ -53,24 +51,25 @@ class HomeFragment : Fragment() {
                 startActivity(intent)
             }
         }
-        return root
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
-    @RequiresApi(Build.VERSION_CODES.R)
-    fun hideSystemBars() {
-        val controller = requireActivity().window.insetsController
-        if (controller != null) {
-            controller.hide(WindowInsets.Type.systemBars())
-            controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-        }
-    }
-    @RequiresApi(Build.VERSION_CODES.R)
-    override fun onResume() {
-        super.onResume()
-        hideSystemBars()
-    }
 }
+
+
+//    @RequiresApi(Build.VERSION_CODES.R)
+//    fun hideSystemBars() {
+//        val controller = requireActivity().window.insetsController
+//        if (controller != null) {
+//            controller.hide(WindowInsets.Type.systemBars())
+//            controller.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        }
+//    }
+//    @RequiresApi(Build.VERSION_CODES.R)
+//    override fun onResume() {
+//        super.onResume()
+//        hideSystemBars()
+//    }
+//}
